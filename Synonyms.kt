42fe7feb3814ce7bb8190d26367b1522c9d9a599
Chance Payne <ChancePayne@users.noreply.github.com>
2019-06-25 14:27:42 -0700

    Create Synonyms.kt

---
+++ Synonyms.kt
@@ -0,0 +1,5 @@
+val synonyms = arrayOf(
+        arrayOf("swift", "abrupt", "expeditious", "hasty", "nimble", "quick", "rapid", "speedy", "sudden", "unexpected"),
+        arrayOf("objective", "detached", "disinterested", "dispassionate", "equitable", "evenhanded", "nonpartisan", "open-minded", "unbiased"),
+        arrayOf("calculate", "adjust", "appraise", "consider", "count", "determine", "forecast", "gauge", "guess", "measure", "multiply", "reckon", "subtract", "tally", "weigh", "work out"),
+        arrayOf("create", "build", "conceive", "constitute", "construct", "design", "devise", "discover", "establish", "forge", "form"))
